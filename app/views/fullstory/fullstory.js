5d0e1de714dd07b041d8fd5f2b79d2ebdd5e5f2b
dameety <djdamilola01@gmail.com>
2016-12-04 23:07:32 +0100

    completed the full story page

---
+++ app/views/fullstory/fullstory.js
@@ -0,0 +1,26 @@
+// var createViewModel = require("./main-view-model").createViewModel;
+var Observable = require("data/observable");
+var ObservableArray = require("data/observable-array");
+var http = require("http");
+var moment = require("moment");
+var frameModule = require("ui/frame")
+
+
+
+var viewModel = new Observable.Observable({    
+	mainTitle: 'Hello User',
+	allItems: new ObservableArray.ObservableArray([])
+});
+ 
+
+exports.onNavigatingTo = function (args) {
+	var page = args.object;
+	page.bindingContext = page.navigationContext;
+
+}
+
+
+
+exports.goBackHome = function () {
+	frameModule.topmost().goBack();
+}
