0fb7193f0a883de696ecf5f8d9ca0f2f228bf263
dameety <djdamilola01@gmail.com>
2016-12-08 20:26:47 +0100

    break subreddits into pages

--- app/views/crazyideas/crazyideas.js
+++ app/views/crazyideas/crazyideas.js
@@ -9,69 +9,36 @@ var page;
 
 var viewModel = new Observable.Observable({    
 	allItems: new ObservableArray.ObservableArray([]),
-	newItems: new ObservableArray.ObservableArray([])
 });
  
 exports.onNavigatingTo = function (args) {
 	page = args.object;
 	page.bindingContext = viewModel;
 	drawer = page.getViewById("drawer");
-	loadReddit();
+	
+	loadCrazyIdeas();
 }
 
 exports.toggleDrawer = function (args) {
 	drawer.toggleDrawerState();
 }
 
-exports.gotoCrazyIdeas = function (args) {
+exports.crazyIdeasTap = function (args) {
 	drawer.closeDrawer();
-	frameModule.topmost().navigate({
-        moduleName: "./views/crazyideas/crazyideas",
-        animated: true,  
-        transition: {
-            duration: 100,
-            curve: "easeIn",
-            name: "slideUp",
-        },
-    });
+	loadCrazyIdeas();
 }
 
-exports.gotoProgramming = function (args) {
-	drawer.closeDrawer();
-	frameModule.topmost().navigate({
-        moduleName: "./views/programming/programming",
-        animated: true,
-        transition: {
-            duration: 100,
-            curve: "easeIn",
-            name: "slideUp",
-        },
-    });
-}
-
-exports.gotoBusiness = function (args) {
-	drawer.closeDrawer();
-	frameModule.topmost().navigate({
-        moduleName: "./views/businees/businees",
-        animated: true,
-        transition: {
-            duration: 100,
-            curve: "easeIn",
-            name: "slideUp",
-        },
-    });
-}
-
-function loadReddit() {
-	http.getJSON("https://www.reddit.com/.json")
+function loadCrazyIdeas () {
+	http.getJSON("https://www.reddit.com/r/CrazyIdeas/.json")
 	.then(function (response) {
+		console.log("crazy ideas loaded")
 		//push each item to the allItems array
 		response.data.children.map(function (item) {
 			item.data.friendlyTime= moment(item.data.created_utc * 1000).fromNow();
 			viewModel.allItems.push(item.data);
 		})
 	})
-}
+}  
 
 exports.storyTap = function (args) {
 	frameModule.topmost().navigate({
