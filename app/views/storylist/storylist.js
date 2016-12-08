640400660595ec6a1756e27f0869f5407c135a7c
dameety <djdamilola01@gmail.com>
2016-12-08 05:57:08 +0100

    added the first category

--- app/views/storylist/storylist.js
+++ app/views/storylist/storylist.js
@@ -3,29 +3,45 @@ var ObservableArray = require("data/observable-array");
 var http = require("http");
 var moment = require("moment");
 var frameModule = require("ui/frame")
-
+var drawer;
+var page;
 
 
 var viewModel = new Observable.Observable({    
 	allItems: new ObservableArray.ObservableArray([])
 });
  
-
 exports.onNavigatingTo = function (args) {
-	var page = args.object;
+	page = args.object;
 	page.bindingContext = viewModel;
-
+	drawer = page.getViewById("drawer");
 	loadReddit();
 }
 
-// exports.chooseCategory = function (args) {
-// 	http.getJSON("h")
-// }
+exports.toggleDrawer = function (args) {
+	drawer.toggleDrawerState();
+}
+
+exports.crazyIdeasTap = function (args) {
+	drawer.closeDrawer();
+	loadCrazyIdeas();	
+}
+
+function loadCrazyIdeas () {
+	http.getJSON("https://www.reddit.com/r/CrazyIdeas/.json")
+	.then(function (response) {
+		console.log("crazy ideas loaded")
+		//push each item to the allItems array
+		response.data.children.map(function (item) {
+			item.data.friendlyTime= moment(item.data.created_utc * 1000).fromNow();
+			viewModel.allItems.push(item.data);
+		})
+	})
+}
 
 function loadReddit() {
 	http.getJSON("https://www.reddit.com/.json")
 	.then(function (response) {
-		
 		//push each item to the allItems array
 		response.data.children.map(function (item) {
 			item.data.friendlyTime= moment(item.data.created_utc * 1000).fromNow();
