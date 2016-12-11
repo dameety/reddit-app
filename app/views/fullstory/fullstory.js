4d68a2f21e195d50208c89aa6886a02634995abe
dameety <djdamilola01@gmail.com>
2016-12-11 01:07:56 +0100

    social share working

--- app/views/fullstory/fullstory.js
+++ app/views/fullstory/fullstory.js
@@ -3,23 +3,26 @@ var ObservableArray = require("data/observable-array");
 var http = require("http");
 var moment = require("moment");
 var frameModule = require("ui/frame")
+var SocialShare = require("nativescript-social-share");
+var page;
 
 
 
-var viewModel = new Observable.Observable({    
-	mainTitle: 'Hello User',
-	allItems: new ObservableArray.ObservableArray([])
-});
- 
+var viewModel; 
 
 exports.onNavigatingTo = function (args) {
-	var page = args.object;
-	page.bindingContext = page.navigationContext;
+	page = args.object;
+	viewModel = page.navigationContext;
+	page.bindingContext = viewModel;
 
 }
 
+exports.shareTap = function (args) {
+	SocialShare.shareText("Check out this page from reddit: " + viewModel.url);
+}
 
 
+// back to story list
 exports.goBackHome = function () {
 	frameModule.topmost().goBack();
 }
